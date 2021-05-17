fecc36e27f4f5c8e75bf8e89eba961108fb4fd0f
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-05-17 19:48:32 -0400

    added login page assets and initial color palette

---
+++ config/colors.js
@@ -0,0 +1,12 @@
+export default {
+    primary: "#21008d",
+    secondary: 'blue',
+    accent: '#green',
+    black: '#111',
+    white: '#eee',
+    light: '#f8f4f4',
+    medium: '#6e6868',
+    dark: '#0c0c0c',
+    danger: '#ff5252'
+
+}
