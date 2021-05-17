5bc795825fa562f04e16e1ac6274d77d26c2e165
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-05-17 19:49:28 -0400

    created reusable AppTextInput component

---
+++ components/AppTextInput.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../config/colors";
+
+function AppTextInput({ icon, ...otherProps }) {
+  return (
+    <View style={styles.container}>
+      {icon && (
+        <MaterialCommunityIcons
+          name={icon}
+          size={30}
+          color={colors.medium}
+          style={styles.icon}
+        />
+      )}
+      <TextInput style={styles.textInput} {...otherProps} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: "#eee",
+    borderRadius: 15,
+    flexDirection: "row",
+    marginVertical: 10,
+    padding: 5,
+    width: "90%",
+  },
+  icon: {
+    marginHorizontal: 10,
+  },
+  textInput: {
+    color: colors.dark,
+    fontSize: 25,
+  },
+});
+
+export default AppTextInput;
