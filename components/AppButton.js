956a2beb60634686ba5849dbbd819a446b4d8993
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-05-18 18:00:26 -0400

    created button component

---
+++ components/AppButton.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import colors from "../config/colors";
+
+function AppButton({title, onPress}) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primary,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5,
+    width: '90%',
+    marginVertical: 10,
+  },
+  text: {
+    color: colors.white,
+    fontSize: 25,
+  }
+});
+
+export default AppButton;
--- components/AppTextInput.js
+++ components/AppTextInput.js
@@ -35,6 +35,7 @@ const styles = StyleSheet.create({
   textInput: {
     color: colors.dark,
     fontSize: 25,
+    width: '100%',
   },
 });
 
