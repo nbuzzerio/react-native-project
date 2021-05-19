a2abc0a54767cdbeb447269a57ae3772b90a73a9
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-05-18 21:49:44 -0400

    refactored main app component

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View, Image, Text, TouchableOpacity } from "react-native";
 
-import AppTextInput from "./components/AppTextInput";
-import colors from './config/colors';
+import AppLogin from "./components/AppLogin";
+import colors from "./config/colors";
 
 export default function App() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
   return (
     <View style={styles.container}>
       <View style={styles.bannerContainer}>
@@ -18,47 +15,24 @@ export default function App() {
         />
         <Image style={styles.banner} source={require("./assets/visneta.png")} />
       </View>
-      <View style={styles.loginContainer}>
-        <AppTextInput
-          onChangeText={(text) => {
-            setUsername(text);
-          }}
-          icon="email"
-          placeholder="Username"
-        />
-        <AppTextInput
-          onChangeText={(text) => {
-            setPassword(text);
-          }}
-          icon="lock"
-          placeholder="Password"
-          secureTextEntry
-        />
-      </View>
+      <AppLogin />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   banner: {
-    height: 75,
+    height: 70,
     position: "absolute",
     top: "40%",
-    width: 300,
+    width: 280,
   },
   bannerContainer: {
     alignItems: "center",
-    height: "65%",
-    width: "100%",
-  },
-  loginContainer: {
-    alignItems: "center",
-    height: "65%",
-    paddingTop: 10,
+    height: "55%",
     width: "100%",
   },
   container: {
-    alignItems: "center",
     backgroundColor: colors.white,
     flex: 1,
     position: "relative",
