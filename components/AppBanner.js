fac160a152a02dd9fa4249125960c2c25cbce953
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-05-20 21:25:26 -0400

    added mock search bar to banner

--- components/AppBanner.js
+++ components/AppBanner.js
@@ -1,10 +1,20 @@
-import React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import colors from "../config/colors";
-import { AntDesign } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { FontAwesome5 } from "@expo/vector-icons";
+import AppTextInput from "./AppTextInput";
 
 function AppBanner() {
+  const [isSearching, setIsSearching] = useState(false);
+  const [query, setQuery] = useState("");
+
   return (
     <View style={styles.banner}>
       <FontAwesome5
@@ -13,16 +23,42 @@ function AppBanner() {
         color={colors.white}
         style={styles.icon}
       />
-      <Image
-        style={styles.logo}
-        source={require("../assets/visneta-white.png")}
-      />
-      <AntDesign
-        name={"search1"}
-        size={30}
-        color={colors.accent}
-        style={styles.icon}
-      />
+      {isSearching && (
+        <View style={styles.search}>
+          <AppTextInput
+            icon="magnify"
+            placeholder="Case # / Addr / City / ZIP"
+            onChangeText={(query) => {
+              setQuery(query);
+            }}
+            onEndEditing={() => {
+              setQuery("");
+            }}
+            returnKeyType="search"
+            value={query}
+            style={{ fontSize: 14, color: colors.dark }}
+          />
+          <Text style={{ color: colors.white, paddingHorizontal: 15 }}>
+            Filter
+          </Text>
+        </View>
+      )}
+      {!isSearching && (
+        <>
+          <Image
+            style={styles.logo}
+            source={require("../assets/visneta-white.png")}
+          />
+          <TouchableWithoutFeedback onPress={() => setIsSearching(true)}>
+            <MaterialCommunityIcons
+              name={"magnify"}
+              size={40}
+              color={colors.accent}
+              style={styles.icon}
+            />
+          </TouchableWithoutFeedback>
+        </>
+      )}
     </View>
   );
 }
@@ -39,11 +75,19 @@ const styles = StyleSheet.create({
   logo: {
     height: "65%",
     marginBottom: 10,
+    marginLeft: 10,
     width: "39%",
     backgroundColor: colors.primary,
   },
   icon: {
     paddingTop: 5,
+  },
+  search: {
+    width: "70%",
+    height: "1000%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
